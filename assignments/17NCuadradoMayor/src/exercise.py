f848625b73324c3d9a2290b32cf40a7d8517e555
Dillan1903 <A01252454@itesm.mx>
2021-09-10 17:49:23 +0000

    17NCuadradoMayor

--- assignments/17NCuadradoMayor/src/exercise.py
+++ assignments/17NCuadradoMayor/src/exercise.py
@@ -1,13 +1,15 @@
-
-
 def main():
     
     #escribe tu código abajo de esta línea
     numero = int(input("Escribe un numero : "))
     ans = 1
+
     while ans**2<=numero:
         ans += 1
-        print(ans)
+    
+    print(ans)
 
 if __name__ == '__main__':
     main()
+
+
